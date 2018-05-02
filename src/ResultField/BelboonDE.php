a8999c5c7efbb3c3b026490a5733ada83da089a8
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-05-02 12:17:22 +0200

    Update BelboonDE.php

--- src/ResultField/BelboonDE.php
+++ src/ResultField/BelboonDE.php
@@ -70,7 +70,7 @@ class BelboonDE extends ResultFields
         /**
          * @var LanguageMutator $languageMutator
          */
-		$languageMutator = pluginApp(LanguageMutator::class, ['language' => [$settings->get('lang')]]);
+		$languageMutator = pluginApp(LanguageMutator::class, ['languages' => [$settings->get('lang')]]);
 
         /**
          * @var DefaultCategoryMutator $defaultCategoryMutator
